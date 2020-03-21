47e323159329eb110aaedad068df4564d194a776
ilhamakhsan <50404822+ilhamakhsan@users.noreply.github.com>
2020-03-21 18:46:47 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,12 @@
+
+public class Mahasiswa {
+    public static void main(String[] args) {
+      String nama = "Ilham Akhsani";
+      String kelas = "4A";
+      String nim = "18090044";
+        System.out.println("Nama : "+nama);
+        System.out.println("Kelas : "+kelas);
+        System.out.println("Nim : "+nim);
+        
+    }
+}
